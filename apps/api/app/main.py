396e0233e68b2140f917ba766eb6642cbc2b0c3b
daria425 <darianaumova5@gmail.com>
2025-02-28 17:32:45 +0200

    structure <3

--- apps/api/app/main.py
+++ apps/api/app/main.py
@@ -1,15 +1,16 @@
 
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.middleware.cors import CORSMiddleware
 from app.db.db_connection import db_connection
 from app.routes import service_status
+import asyncio
 async def lifespan(app: FastAPI):
     await db_connection.connect()
     yield
     await db_connection.close()
 
 app=FastAPI(lifespan=lifespan)
-
+clients=[]
 origins=[
   "http://localhost:5173",
 ]
@@ -27,4 +28,16 @@ def root():
         "message": "Hello from FastAPI"
     }
 
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    clients.append(websocket)
+    try:
+        while True:
+            await asyncio.sleep(1)
+            for client in clients:
+                await client.send_text("Service Status Update")
+    except WebSocketDisconnect:
+        clients.remove(websocket)
+        
 app.include_router(service_status.router)
